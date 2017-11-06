6d1b879eaf8bfd3b73a008808c25b1e34b5a7581
Franklin Antony <franklinanton.007@gmail.com>
2017-11-07 01:08:30 +0530

    Updated pic_controller.rb

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,14 +1,24 @@
 class PicsController < ApplicationController
-
+  before_action : find_pic, only: [:show, :edit, :update, :destroy]
+  
   def index
 
   end
 
+  def show
+    
   def new
     @pic = Pic.new
   end
+
   def create
     @pic = Pic.new(pic_params)
+
+    if @pic.save
+      redirect_to @pic, notice: "Posted Successfully!!"
+    else
+      render 'new'
+    end
 
   end
 
@@ -16,5 +26,10 @@ class PicsController < ApplicationController
   def pic_params
     params.require(:pic).permit(:title, :description)
   end
+
+  def fimd_pic
+    @pic= Pic.find(params[:id])
+  end
+ 
 
 end
